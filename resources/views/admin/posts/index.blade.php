201f1194eb63d33279badf351274b2baa8035ae4
pelopeno <erpeloluis@gmail.com>
2025-10-01 17:46:28 +0800

    test

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="p-8 bg-gray-100 min-h-screen">
-    <h1 class="text-2xl font-bold mb-6">📑 Posts</h1>
+    <h1 class="text-2xl font-bold mb-6">Posts</h1>
 
     {{-- Success message --}}
     @if(session('success'))
