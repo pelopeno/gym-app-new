2b2c72395547c14c206e13034bdf2a740f94f23f
pelopeno <erpeloluis@gmail.com>
2025-10-01 14:28:12 +0800

    removed some models

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,11 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\Announcement;
-use App\Models\Review;
 
 class AdminController extends Controller
 {
